f727b9a71931af2601b887c920ddac96e9f98b02
Miguel García Reimers <mgarciareimers@gmail.com>
2023-10-31 19:27:48 +0100

    Start of Yes/No app.

---
+++ yes_no_app/lib/main.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({
+    Key? key
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Yes No App',
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text(
+            'Material App Bar'
+          ),
+        ),
+        body: Center(
+          child: FilledButton.tonal(
+            onPressed: () {},
+            child: const Text(
+              'Click me'
+            )
+          ),
+        ),
+      ),
+    );
+  }
+}
